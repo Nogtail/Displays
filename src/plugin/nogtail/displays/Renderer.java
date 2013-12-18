8fa7b9380f5a8f1c639796ad1fc888d8b98112f5
Nogtail <dalygame@gmail.com>
2013-12-19 09:59:33 +1100

    Attempted to fix eclipse formatting issues.

--- src/plugin/nogtail/displays/Renderer.java
+++ src/plugin/nogtail/displays/Renderer.java
@@ -2,11 +2,23 @@ package plugin.nogtail.displays;
 
 import org.bukkit.entity.Player;
 import org.bukkit.map.MapCanvas;
+import org.bukkit.map.MapPalette;
 import org.bukkit.map.MapRenderer;
 import org.bukkit.map.MapView;
+import org.bukkit.map.MinecraftFont;
 
 public class Renderer extends MapRenderer {
+	boolean updateNeeded = true;
+
+	public Renderer() {
+		super(true);
+	}
+
 	public void render(MapView view, MapCanvas canvas, Player player) {
-		// TODO
+		if (updateNeeded) {
+			canvas.drawText(10, 10, MinecraftFont.Font, "§" + MapPalette.BLUE + ";Hi there\n§" + MapPalette.RED + ";I am a map\n§" + MapPalette.DARK_GREEN + ";And you are a human!");
+			updateNeeded = false;
+			player.sendMap(view);
+		}
 	}
 }
